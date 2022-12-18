6cc28b9c442dd472c4ac2c4b40b1959de7dc220c
Olha Hranevska <107428487+OlhaGr@users.noreply.github.com>
2022-12-18 18:48:47 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import './App.css';
+import  React, { useState, useEffect } from 'react' ;
+import axios from 'axios';
 
 function App() {
+   const [pages, setPages] = useState([]);
+   const [repo, setRepo] = useState([]);
+   const photo = pages.avatar_url;
+
+   useEffect(() => {
+    axios.get('https://api.github.com/users/OlhaGr?client_id=a1a5a13b1e7446f4e730&client_secret=5b4cf0f151ef1ff324ea903ae0146d35d61e26d7&sort=created')
+      .then((res) => { 
+        setPages(res.data);
+      })
+      .catch ((err) =>
+        console.log(err));
+    }, []);
+
+   useEffect(() => {
+    axios.get('https://api.github.com/users/OlhaGr/repos')
+      .then((repo) =>
+        setRepo(repo.data)
+      )
+      .catch ((err) =>
+      console.log(err));
+    });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div id='user'>
+          <div id='pic'><img src={photo} alt="photo" /></div>
+          <div id='userinfo'>
+           <li>Full Name: {pages.name}</li>
+            <li>User Name: {pages.login}</li>
+             <li>Email Address: {pages.email}</li>
+             <li>Location: {pages.location}</li>
+          </div>
+      </div>
+        <div id='repos'>
+          <h3>Repositories</h3>
+          <ul>
+            {repo.map((item) => {
+              return <li key={item.id}><a href={'https://github.com/' + item.full_name}>{item.name}</a></li>
+            })}
+          </ul>
+        </div>
     </div>
   );
 }
